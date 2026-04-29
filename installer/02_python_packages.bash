70973e21dc2d1e227a60729254b640e715d71aa5
Fabián Robledo <fabianry97@gmail.com>
2026-04-29 12:55:37 +0200

    FIX: apt commands do not have non-interactive run

--- installer/02_python_packages.bash
+++ installer/02_python_packages.bash
@@ -17,22 +17,22 @@ echo "!!!!!!!     BEWARE: INSTALLATION REQUIRES SUDO RIGHTS       !!!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
 
-apt install python3-numpy
-apt install python3-scipy
-apt install python3-matplotlib 
-apt install snakemake 
-apt install python3-rpy2 
-apt install python3-pysimplesoap 
-apt install python3-requests 
-apt install python3-yaml
-apt install python3-wsdl
-apt install python3-wstools
-apt install python3-suds
-apt install python3-venv
-apt install python3-pip
-# apt install python3-fisher ## 2025-12-29: unable to install
-apt install python3-pygraphviz
-apt install python3-requests   # required for downloading GO terms from biomart
+apt install -y python3-numpy
+apt install -y python3-scipy
+apt install -y python3-matplotlib 
+apt install -y snakemake 
+apt install -y python3-rpy2 
+apt install -y python3-pysimplesoap 
+apt install -y python3-requests 
+apt install -y python3-yaml
+apt install -y python3-wsdl
+apt install -y python3-wstools
+apt install -y python3-suds
+apt install -y python3-venv
+apt install -y python3-pip
+# apt install -ypython3-fisher ## 2025-12-29: unable to install
+apt install -y python3-pygraphviz
+apt install -y python3-requests   # required for downloading GO terms from biomart
 
 ## Check disk usage
 df -m > ${RSAT}/install_logs/df_$(date +%Y-%m-%d_%H-%M-%S)_pip_libraries_installed.txt
